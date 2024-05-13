9374264767b184b36182ee247adeda917fb7f60b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-13 12:38:48 -0500

    feat (YouTubeForm.tsx): carga de datos por defecto
    Se implementa carga de datos por defecto al formulario pasando un objeto al hook con los parámetros del mismo. Adicionalmente, se implementa un ejemplo de consulta a un API Rest para mapear el dato del correo.

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-import { DevTool } from "@hookform/devtools";
+import { DevTool } from '@hookform/devtools';
 
 let renderCount = 0;
 
@@ -10,7 +10,20 @@ type FormValues = {
 };
 
 export const YouTubeForm = () => {
-    const form = useForm<FormValues>();
+    const form = useForm<FormValues>( {
+        // El objeto de configuración permite pasar valores por defecto
+        defaultValues: async () => {
+
+            const response = await fetch( 'https://jsonplaceholder.typicode.com/users/1' );
+            const data = await response.json();
+
+            return {
+                username: 'Batman',
+                email: data?.email,
+                channel: ''
+            };
+        }
+    } );
 
     const {
         register,
@@ -24,7 +37,7 @@ export const YouTubeForm = () => {
     } = formState;
 
     const onSubmit = ( data: FormValues ) => {
-        console.log( "Form submitted", data );
+        console.log( 'Form submitted', data );
     };
 
     return (
@@ -33,57 +46,57 @@ export const YouTubeForm = () => {
             <h1>YouTube Form</h1>
 
             <form onSubmit={ handleSubmit( onSubmit ) } noValidate>
-                <div className="form-control">
-                    <label htmlFor="username">Username</label>
+                <div className='form-control'>
+                    <label htmlFor='username'>Username</label>
                     <input
-                        type="text"
-                        id="username"
-                        { ...register( "username", {
-                            required: { value: true, message: "Username is required" },
+                        type='text'
+                        id='username'
+                        { ...register( 'username', {
+                            required: { value: true, message: 'Username is required' },
                         } ) }
                     />
-                    <p className="error">{ errors.username?.message }</p>
+                    <p className='error'>{ errors.username?.message }</p>
                 </div>
 
-                <div className="form-control">
-                    <label htmlFor="email">E-mail</label>
+                <div className='form-control'>
+                    <label htmlFor='email'>E-mail</label>
                     <input
-                        type="email"
-                        id="email"
-                        { ...register( "email", {
+                        type='email'
+                        id='email'
+                        { ...register( 'email', {
                             pattern: {
                                 value:
                                     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                                message: "Invalid email format",
+                                message: 'Invalid email format',
                             },
                             validate: {
                                 notAdmin: ( fieldValue ) => {
                                     // Si no pasa la validación retorna el mensaje de error
-                                    return fieldValue !== "admin@example.com" || "Enter a different email address";
+                                    return fieldValue !== 'admin@example.com' || 'Enter a different email address';
                                 },
                                 notBlacklisted: ( fieldValue ) => {
                                     // Si no pasa la validación retorna el mensaje de error
-                                    return !fieldValue.endsWith( "baddomain.com" ) || "This domain is not supported";
+                                    return !fieldValue.endsWith( 'baddomain.com' ) || 'This domain is not supported';
                                 }
                             }
                         } ) }
                     />
-                    <p className="error">{ errors.email?.message }</p>
+                    <p className='error'>{ errors.email?.message }</p>
                 </div>
 
-                <div className="form-control">
-                    <label htmlFor="channel">Channel</label>
+                <div className='form-control'>
+                    <label htmlFor='channel'>Channel</label>
                     <input
-                        type="text"
-                        id="channel"
-                        { ...register( "channel", {
+                        type='text'
+                        id='channel'
+                        { ...register( 'channel', {
                             required: {
                                 value: true,
-                                message: "Channel is required"
+                                message: 'Channel is required'
                             },
                         } ) }
                     />
-                    <p className="error">{ errors.channel?.message }</p>
+                    <p className='error'>{ errors.channel?.message }</p>
                 </div>
 
                 <button>Submit</button>

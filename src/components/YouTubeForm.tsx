a4f18847e78e03531efa199f3a1c389552005672
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-13 16:54:36 -0500

    feat (YouTubeForm.tsx): async form validation
    Se implementa validación asíncrona para verificar si el correo existe en la BD

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -166,6 +166,12 @@ export const YouTubeForm = () => {
                                 notBlacklisted: ( fieldValue ) => {
                                     // Si no pasa la validación retorna el mensaje de error
                                     return !fieldValue.endsWith( 'baddomain.com' ) || 'This domain is not supported';
+                                },
+                                emailAvailable: async ( fieldValue ) => {
+                                    const response = await fetch( `https://jsonplaceholder.typicode.com/users?email=${ fieldValue }` );
+                                    const data = await response.json();
+
+                                    return data.length === 0 || 'Email already exists';
                                 }
                             }
                         } ) }
